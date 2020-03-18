f6a4641f13e7a67cb0fcdeffb1f22b890f1905e8
DmitriySalnikov <salnikov.mine@yandex.ru>
2020-03-18 12:39:22 +0300

    Add project files.

---
+++ TilesetFixer/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace TilesetFixer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button3 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.nud_HorisontalSpacing = new System.Windows.Forms.NumericUpDown();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.nud_width = new System.Windows.Forms.NumericUpDown();
+			this.label5 = new System.Windows.Forms.Label();
+			this.nud_height = new System.Windows.Forms.NumericUpDown();
+			this.label6 = new System.Windows.Forms.Label();
+			this.nud_VerticalSpacing = new System.Windows.Forms.NumericUpDown();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.nud_HorisontalSpacing)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_width)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_height)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_VerticalSpacing)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Original Tileset:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(15, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(296, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(317, 23);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Browse..";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(317, 62);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Browse..";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(15, 64);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(296, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Output Folder:";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(15, 191);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(376, 23);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "Fixe Tileset";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(15, 168);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(127, 17);
+			this.checkBox1.TabIndex = 7;
+			this.checkBox1.Text = "Add Suffix to fixed file";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// nud_HorisontalSpacing
+			// 
+			this.nud_HorisontalSpacing.Location = new System.Drawing.Point(15, 142);
+			this.nud_HorisontalSpacing.Name = "nud_HorisontalSpacing";
+			this.nud_HorisontalSpacing.Size = new System.Drawing.Size(69, 20);
+			this.nud_HorisontalSpacing.TabIndex = 8;
+			this.nud_HorisontalSpacing.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(136, 126);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(110, 13);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Vertical Tiles spacing:";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 87);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(60, 13);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Tiles width:";
+			// 
+			// nud_width
+			// 
+			this.nud_width.Location = new System.Drawing.Point(15, 103);
+			this.nud_width.Name = "nud_width";
+			this.nud_width.Size = new System.Drawing.Size(71, 20);
+			this.nud_width.TabIndex = 10;
+			this.nud_width.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(136, 87);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(64, 13);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "Tiles height:";
+			// 
+			// nud_height
+			// 
+			this.nud_height.Location = new System.Drawing.Point(139, 103);
+			this.nud_height.Name = "nud_height";
+			this.nud_height.Size = new System.Drawing.Size(71, 20);
+			this.nud_height.TabIndex = 12;
+			this.nud_height.Value = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(12, 126);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(122, 13);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "Horisontal Tiles spacing:";
+			// 
+			// nud_VerticalSpacing
+			// 
+			this.nud_VerticalSpacing.Location = new System.Drawing.Point(141, 142);
+			this.nud_VerticalSpacing.Name = "nud_VerticalSpacing";
+			this.nud_VerticalSpacing.Size = new System.Drawing.Size(69, 20);
+			this.nud_VerticalSpacing.TabIndex = 14;
+			this.nud_VerticalSpacing.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(404, 228);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.nud_VerticalSpacing);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.nud_height);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.nud_width);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.nud_HorisontalSpacing);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Tileset Fixer";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+			this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+			this.DragLeave += new System.EventHandler(this.Form1_DragLeave);
+			((System.ComponentModel.ISupportInitialize)(this.nud_HorisontalSpacing)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_width)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_height)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nud_VerticalSpacing)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.NumericUpDown nud_HorisontalSpacing;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown nud_width;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.NumericUpDown nud_height;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.NumericUpDown nud_VerticalSpacing;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ TilesetFixer/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Drawing.Imaging;
+
+namespace TilesetFixer
+{
+	public partial class Form1 : Form
+	{
+		List<string> supportedFormats = new List<string>
+		{
+			".png",
+			".jpg",
+			".jpeg",
+			".bmp",
+		};
+
+		const string ConfigFile = "setting.json";
+
+		class Config
+		{
+			[JsonProperty("output")]
+			public string OutputPath = "";
+
+			[JsonProperty("add_suffix")]
+			public bool AddSuffix = true;
+
+			[JsonProperty("width")]
+			public int Width = 32;
+
+			[JsonProperty("height")]
+			public int Height = 32;
+
+			[JsonProperty("hor_spacing")]
+			public int HorisontalSpacing = 1;
+
+			[JsonProperty("ver_spacing")]
+			public int VerticalSpacing = 1;
+		}
+
+		Config config = new Config();
+
+		public Form1()
+		{
+			InitializeComponent();
+			LoadConfig();
+
+			string filter = "All supported images|";
+			for (int i = 0; i < supportedFormats.Count; i++)
+			{
+				filter += $"*{supportedFormats[i]}";
+				if (i != supportedFormats.Count - 1)
+					filter += ";";
+			}
+			filter += "|";
+
+			for (int i = 0; i < supportedFormats.Count; i++)
+			{
+				filter += $"*{supportedFormats[i]}|*{supportedFormats[i]}";
+				if (i != supportedFormats.Count - 1)
+					filter += "|";
+			}
+
+			openFileDialog1.Filter = filter;
+		}
+
+		private void LoadConfig()
+		{
+			try
+			{
+				if (File.Exists(ConfigFile))
+					config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigFile));
+				textBox2.Text = config.OutputPath;
+				checkBox1.Checked = config.AddSuffix;
+				nud_HorisontalSpacing.Value = config.HorisontalSpacing;
+				nud_VerticalSpacing.Value = config.VerticalSpacing;
+				nud_width.Value = config.Width;
+				nud_height.Value = config.Height;
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+
+		private void SaveConfig()
+		{
+			try
+			{
+				config.OutputPath = textBox2.Text;
+				config.AddSuffix = checkBox1.Checked;
+				config.HorisontalSpacing = (int)nud_HorisontalSpacing.Value;
+				config.VerticalSpacing = (int)nud_VerticalSpacing.Value;
+				config.Width = (int)nud_width.Value;
+				config.Height = (int)nud_height.Value;
+
+				var str = JsonConvert.SerializeObject(config);
+				File.WriteAllText(ConfigFile, str);
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+
+		private void Form1_DragEnter(object sender, DragEventArgs e)
+		{
+			string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
+			if (data != null && data.Length > 0 && supportedFormats.Contains(Path.GetExtension(data[0])) && File.Exists(data[0]))
+				e.Effect = DragDropEffects.Move;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
+		private void Form1_DragLeave(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] data = (string[])e.Data.GetData(DataFormats.FileDrop);
+			if (data != null && data.Length > 0)
+			{
+				textBox1.Text = data[0];
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SaveConfig();
+			try
+			{
+				if (File.Exists(textBox1.Text))
+				{
+					if (!Directory.Exists(textBox2.Text))
+						Directory.CreateDirectory(textBox2.Text);
+
+					var name = "";
+					var outtex = "";
+
+					if (checkBox1.Checked)
+						name = Path.GetFileNameWithoutExtension(textBox1.Text) + "_fixed" + Path.GetExtension(textBox1.Text);
+					else
+						name = Path.GetFileName(textBox1.Text);
+					outtex = Path.Combine(textBox2.Text, name);
+
+					FixTilesetTexture(textBox1.Text, outtex);
+				}
+				else
+				{
+					MessageBox.Show($"\"{textBox1.Text}\" does not exists!", "Error");
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error!!!\n" + ex.Message, "Error");
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+				textBox1.Text = openFileDialog1.FileName;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (Directory.Exists(textBox2.Text))
+				folderBrowserDialog1.SelectedPath = textBox2.Text;
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				textBox2.Text = folderBrowserDialog1.SelectedPath;
+			}
+		}
+
+		private PixelFormat _fixIamgeFormat(PixelFormat format)
+		{
+			switch (format)
+			{
+				case PixelFormat.Format1bppIndexed:
+					return PixelFormat.Format32bppRgb;
+				case PixelFormat.Format4bppIndexed:
+					return PixelFormat.Format32bppRgb;
+				case PixelFormat.Format8bppIndexed:
+					return PixelFormat.Format32bppRgb;
+			}
+
+			return format;
+		}
+
+		struct LineAndCopies
+		{
+			public int line;
+			public int start;
+			public int end;
+		}
+
+		private void FixTilesetTexture(string tex, string outTex)
+		{
+
+			var img = Image.FromFile(tex);
+
+			var xSize = (int)Math.Ceiling(img.Width / (float)config.Width);
+			var ySize = (int)Math.Ceiling(img.Height / (float)config.Height);
+			var xHalfSpace = (int)Math.Ceiling((float)config.HorisontalSpacing / 2);
+			var yHalfSpace = (int)Math.Ceiling((float)config.VerticalSpacing / 2);
+
+			var newImg = new Bitmap(
+				img.Width + ((xSize - 1) * config.HorisontalSpacing),
+				img.Height + ((ySize - 1) * config.VerticalSpacing),
+				_fixIamgeFormat(img.PixelFormat));
+
+			List<LineAndCopies> hLines = new List<LineAndCopies>();
+			List<LineAndCopies> vLines = new List<LineAndCopies>();
+
+			var newImgDev = Graphics.FromImage(newImg);
+			for (int x = 0; x < xSize; x++)
+			{
+				for (int y = 0; y < ySize; y++)
+				{
+					var tileStartX = config.Width * x + config.HorisontalSpacing * x;
+					var tileStartY = config.Height * y + config.VerticalSpacing * y;
+
+					newImgDev.DrawImage(img,
+						new Rectangle(tileStartX, tileStartY, config.Width, config.Height),
+						new Rectangle(config.Width * x, config.Height * y, config.Width, config.Height),
+						GraphicsUnit.Pixel);
+				}
+			}
+
+			for (int x = 0; x < xSize; x++)
+			{
+				var tileStartX = config.Width * x + config.HorisontalSpacing * x;
+				var tileEndX = tileStartX + config.Width - 1;
+
+				hLines.Add(new LineAndCopies() { start = tileStartX - xHalfSpace, end = tileStartX - 1, line = tileStartX });
+				hLines.Add(new LineAndCopies() { start = tileEndX + 1, end = tileEndX + xHalfSpace, line = tileEndX });
+			}
+
+			for (int y = 0; y < ySize; y++)
+			{
+				var tileStartY = config.Height * y + config.VerticalSpacing * y;
+				var tileEndY = tileStartY + config.Height - 1;
+
+				vLines.Add(new LineAndCopies() { start = tileStartY - yHalfSpace, end = tileStartY - 1, line = tileStartY });
+				vLines.Add(new LineAndCopies() { start = tileEndY + 1, end = tileEndY + yHalfSpace, line = tileEndY });
+			}
+
+
+			foreach (var l in hLines)
+			{
+				for (int x = l.start; x <= l.end; x++)
+				{
+					newImgDev.DrawImage(newImg,
+						new Rectangle(x, 0, 1, newImg.Height),
+						new Rectangle(l.line, 0, 1, newImg.Height),
+						GraphicsUnit.Pixel);
+				}
+			}
+
+			foreach (var l in vLines)
+			{
+				for (int y = l.start; y <= l.end; y++)
+				{
+					newImgDev.DrawImage(newImg,
+						new Rectangle(0, y, newImg.Width, 1),
+						new Rectangle(0, l.line, newImg.Width, 1),
+						GraphicsUnit.Pixel);
+				}
+			}
+
+			newImg.Save(outTex);
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			SaveConfig();
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			SaveConfig();
+		}
+	}
+}
